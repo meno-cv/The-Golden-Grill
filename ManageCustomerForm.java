f04a341ad8e520ae2ec73c4737d6391c95921fa3
Meno <menolycasch2006@gmail.com>
2026-08-02 19:54:54 +0530

    Commit 18

---
+++ ManageCustomerForm.java
@@ -0,0 +1,15 @@
+package thegoldengrill.view;
+
+import thegoldengrill.controller.CustomerFormController;
+import thegoldengrill.model.Customer;
+
+/**
+ *
+ * @author menob
+ */
+public class ManageCustomerForm extends javax.swing.JFrame {
+
+    public ManageCustomerForm() {
+        initComponents();
+    }
+
