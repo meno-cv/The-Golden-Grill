a647e0f7eb2cdea015176d9e97af33634c80ddcd
Meno <menolycasch2006@gmail.com>
2026-08-02 19:55:32 +0530

    Commit 19

--- ManageCustomerForm.java
+++ ManageCustomerForm.java
@@ -13,3 +13,27 @@ public class ManageCustomerForm extends javax.swing.JFrame {
         initComponents();
     }
 
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtCustomerId = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        txtCustomerName = new javax.swing.JTextField();
+        txtCustomerAddress = new javax.swing.JTextField();
+        txtCustomerContact = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        btnAddCustomerOnAction = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel7 = new javax.swing.JLabel();
+
