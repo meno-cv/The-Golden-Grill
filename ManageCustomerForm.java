1145c594970c0c1ce029a0b0f0c73d522e0faf34
Meno <menolycasch2006@gmail.com>
2026-08-04 23:47:09 +0530

    Commit 27

--- ManageCustomerForm.java
+++ ManageCustomerForm.java
@@ -110,3 +110,16 @@ public class ManageCustomerForm extends javax.swing.JFrame {
             },
 
 
+             new String [] {
+                "ID", "Name", "Address", "Contact  No"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+
