4af50c91e76e2ebf596e967e657b5504258ab007
Meno <menolycasch2006@gmail.com>
2026-08-12 20:02:48 +0530

    Commit 32 - Search Customer Form

---
+++ SearchCustomerForm.java
@@ -0,0 +1,28 @@
+package thegoldengrill.view;
+
+/**
+ *
+ * @author menob
+ */
+public class SearchCustomerForm extends javax.swing.JFrame {
+
+   
+    public SearchCustomerForm() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
