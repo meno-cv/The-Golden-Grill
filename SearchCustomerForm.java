b4721ddbb38fd9377f198f274dbcdc6463499dca
Meno <menolycasch2006@gmail.com>
2026-08-12 20:04:11 +0530

    Commit 34

--- SearchCustomerForm.java
+++ SearchCustomerForm.java
@@ -38,3 +38,23 @@ public class SearchCustomerForm extends javax.swing.JFrame {
         jButton1.setText("Search");
 
         jLabel3.setText("Customer Name : ");
+
+         jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Order ID", "QTY", "Status", "Value"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
