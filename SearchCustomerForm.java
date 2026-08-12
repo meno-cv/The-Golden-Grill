b19f1f0e77ef6a33be4ffb8abc69a201422a4920
Meno <menolycasch2006@gmail.com>
2026-08-12 20:03:28 +0530

    Commit 33

--- SearchCustomerForm.java
+++ SearchCustomerForm.java
@@ -6,13 +6,11 @@ package thegoldengrill.view;
  */
 public class SearchCustomerForm extends javax.swing.JFrame {
 
-   
     public SearchCustomerForm() {
         initComponents();
     }
 
-    
-    @SuppressWarnings("unchecked")
+@SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
@@ -26,3 +24,17 @@ public class SearchCustomerForm extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 0, 51));
+        jLabel1.setText("Search Customer ");
+
+        jLabel2.setText("Customer ID : ");
+
+        jTextField1.setText("Enter ID");
+
+        jButton1.setText("Search");
+
+        jLabel3.setText("Customer Name : ");
