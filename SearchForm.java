53f3f9f8685ce6522e448036b7cd6525c9f29f05
Meno <menolycasch2006@gmail.com>
2026-08-16 22:33:35 +0530

    Commit 41

---
+++ SearchForm.java
@@ -0,0 +1,25 @@
+package thegoldengrill.view;
+
+/**
+ *
+ * @author menob
+ */
+public class SearchForm extends javax.swing.JFrame {
+
+    public SearchForm() {
+        initComponents();
+    }
+
+
+    @SuppressWarnings("unchecked")
+                        
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        lblSearch = new javax.swing.JLabel();
+        btnSearchOrder = new javax.swing.JButton();
+        btnSearchCustomer = new javax.swing.JButton();
+        btnBestCustomer = new javax.swing.JButton();
+        btnViewOrders = new javax.swing.JButton();
+        btnUpdateOrder = new javax.swing.JButton();
+        btnBack = new javax.swing.JButton();
